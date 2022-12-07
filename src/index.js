df43dc5307deda4ac5aff40709c1280a2bfe5cfb
unknown <kashif_saleem_23@hotmail.com>
2022-12-07 20:15:47 +0500

    add files

--- src/index.js
+++ src/index.js
@@ -6,49 +6,76 @@
 // import _ from 'lodash';
 import './style.css';
 import html from './template.html';
-import Icon from './images/speaker-background.png';
 
-const arr = [
-  {
-    description: 'wash the dishes',
-    completed: 'true',
-    id: 1,
-  },
-  {
-    description: 'cook food',
-    completed: 'false',
-    id: 2,
-  },
-  {
-    description: 'do laundry',
-    completed: 'false',
-    id: 3,
-  },
-];
+const taskInput = document.querySelector('.placeholder');
+const taskBox = document.getElementById('text-content');
 
-// eslint-disable-next-line no-return-assign
-arr.map((card) => document.getElementById('text-content').innerHTML
-  += `<section class="checkbox-label-wrapper">
-        <div class="description">
-          <input type="checkbox" id="checkbox" name="checkbox" value="checkbox">
-          <label for="checkbox" id="checkbox-1" class="label">${card.description}</label>
-        </div>
-        <span class="dots">&#8942</span>
-       </section>`);
+// geting localstorage todo-list
+let todos = JSON.parse(localStorage.getItem('todo-list'));
 
-// function component() {
-//   const element = document.createElement('div');
+const myFunction = (s) => {
+  console.log(s);
+};
 
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-//   // Add the image to our existing div.
-//   const myIcon = new Image();
-//   myIcon.src = Icon;
+// displaying on UI
+const showTodo = () => {
+  let li = '';
+  if (todos) {
+    todos.forEach((todo, id) => {
+      li += `<section class="checkbox-label-wrapper">
+      <div class="description">
+        <input onclick="myFunction()" type="checkbox" id="${id}" name="checkbox" value="checkbox">
+        <label for="${id}" id="checkbox-1" class="label">${todo.name}</label>
+      </div>
+      <span class="dots">&#8942</span>
+     </section>`;
+    });
+    taskBox.innerHTML = li;
+  }
+};
 
-//   element.appendChild(myIcon);
+// showTodo();
 
-//   return element;
-// }
+myFunction();
 
-// document.body.appendChild(component());
+taskInput.addEventListener('keyup', (e) => {
+  const userTask = taskInput.value.trim();
+  if (e.key === 'Enter' && userTask) {
+    if (!todos) { // if todos dosen't exist, pass an empty array
+      todos = [];
+    }
+    taskInput.value = ''; // emty input value
+    const taskInfo = { name: userTask, status: 'pending' };
+    todos.push(taskInfo); // adding new task todos
+    localStorage.setItem('todo-list', JSON.stringify(todos));
+    showTodo();
+  }
+});
+
+// const arr = [
+//   {
+//     description: 'wash the dishes',
+//     completed: 'true',
+//     id: 1,
+//   },
+//   {
+//     description: 'cook food',
+//     completed: 'false',
+//     id: 2,
+//   },
+//   {
+//     description: 'do laundry',
+//     completed: 'false',
+//     id: 3,
+//   },
+// ];
+
+// // eslint-disable-next-line no-return-assign
+// arr.map((card) => document.getElementById('text-content').innerHTML
+//   += `<section class="checkbox-label-wrapper">
+//         <div class="description">
+//           <input type="checkbox" id="checkbox" name="checkbox" value="checkbox">
+//           <label for="checkbox" id="checkbox-1" class="label">${card.description}</label>
+//         </div>
+//         <span class="dots">&#8942</span>
+//        </section>`);
