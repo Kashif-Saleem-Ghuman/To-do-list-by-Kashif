02664e7cd45cdc57a0727f0760b62fb710299d91
unknown <kashif_saleem_23@hotmail.com>
2022-12-07 11:12:00 +0500

    add linter correcton

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,55 @@
 /* eslint-disable linebreak-style */
 /* "import/no-unresolved": "off" */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { resolve } from 'path';
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: path.resolve(__dirname, './src/index.js'),
-  },
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    assetModuleFilename: 'src//images/[name].[ext]',
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: 'images/',
-            },
+export const mode = 'development';
+export const entry = {
+  index: resolve(__dirname, './src/index.js'),
+};
+export const devServer = {
+  static: './dist',
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/template.html',
+  }),
+];
+export const output = {
+  filename: '[name].js',
+  path: resolve(__dirname, 'dist'),
+  clean: true,
+  assetModuleFilename: 'src//images/[name].[ext]',
+};
+export const optimization = {
+  runtimeChunk: 'single',
+};
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            publicPath: 'images/',
           },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader'],
-      },
-    ],
-  },
+        },
+      ],
+    },
+    {
+      test: /\.html$/,
+      use: ['html-loader'],
+    },
+  ],
 };
