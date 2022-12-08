0e7a4c1a2592facf0f23a39b50c70d5d34b99564
unknown <kashif_saleem_23@hotmail.com>
2022-12-08 11:14:00 +0500

    add functions

--- src/index.js
+++ src/index.js
@@ -13,30 +13,73 @@ const taskBox = document.getElementById('text-content');
 // geting localstorage todo-list
 let todos = JSON.parse(localStorage.getItem('todo-list'));
 
-const myFunction = (s) => {
-  console.log(s);
-};
-
 // displaying on UI
 const showTodo = () => {
   let li = '';
   if (todos) {
     todos.forEach((todo, id) => {
+      // if todo status is completed, set the isCompleted value to checked
+      const isCompleted = todo.status === 'completed' ? 'checked' : '';
       li += `<section class="checkbox-label-wrapper">
       <div class="description">
-        <input onclick="myFunction()" type="checkbox" id="${id}" name="checkbox" value="checkbox">
-        <label for="${id}" id="checkbox-1" class="label">${todo.name}</label>
+        <input onclick=updateStatus(this)  type="checkbox" id="${id}" ${isCompleted} name="checkbox" value="checkbox">
+        <label for="${id}" id="checkbox-1" class="${isCompleted} label">${todo.name}</label>
       </div>
-      <span class="dots">&#8942</span>
+      <div class="settings">
+        <span onclick="showMenu(this)" class="dots">&#8942</span>
+        <ul class="task-menu">
+          <li onclick='editTask(${id}, "${todo.name}")' ><i class="uil uil-pen"></i>Edit</li>
+          <li onclick="deleteTask(${id})"><i class="uil uil-trash"></i>Delete</li>
+        </ul>
+      </div> 
      </section>`;
     });
     taskBox.innerHTML = li;
   }
 };
 
-// showTodo();
+showTodo();
 
-myFunction();
+// to delete and edit icon
+window.showMenu = (selectedTask) => {
+  // getting task menu div
+  const taskMenu = selectedTask.nextElementSibling;
+  taskMenu.classList.add('show');
+  document.addEventListener('click', (e) => {
+    // need to add condition for e.target.tagName != "I" ;
+    if (e.target !== selectedTask) {
+      taskMenu.classList.remove('show');
+    }
+  });
+};
+
+// to edit task
+window.editTask = (taskId, taskName) => {
+  console.log(taskId, taskName);
+};
+
+// to delete task
+window.deleteTask = (deleteId) => {
+  // removing selected task from array(todos)
+  todos.splice(deleteId, 1);
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+  showTodo();
+};
+
+window.updateStatus = (selectedTask) => {
+  // getting paragraph that contains task name
+  const taskName = selectedTask.nextElementSibling;
+  if (selectedTask.checked) {
+    taskName.classList.add('checked');
+    // updating the status of selected task to completed
+    todos[selectedTask.id].status = 'completed';
+  } else {
+    taskName.classList.remove('checked');
+    // updating the status of selected task to completed
+    todos[selectedTask.id].status = 'pending';
+  }
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+};
 
 taskInput.addEventListener('keyup', (e) => {
   const userTask = taskInput.value.trim();
