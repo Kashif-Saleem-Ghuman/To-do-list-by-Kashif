87cf37846b892f6c10180e9a89abc58aa52bf2d1
unknown <kashif_saleem_23@hotmail.com>
2022-12-15 19:16:02 +0500

    add test for edit and update status function

--- src/delete.test.js
+++ src/delete.test.js
@@ -1,7 +1,9 @@
 /**
  * @jest-environment jsdom
  */
-import { todos, addTask, deleteTask } from './todoFunc.js';
+import {
+  todos, addTask, deleteTask, toDOList, editTask, updateStatus,
+} from './todoFunc.js';
 
 describe('Add and Remove functions testing', () => {
   document.body.innerHTML = ` <section class="container">
@@ -39,5 +41,18 @@ describe('Add and Remove functions testing', () => {
     deleteTask(1, 1);
     const expected = ['wash dirty clothes', 'Dry'];
     expect(todos).toEqual(expected);
+  });
+});
+
+describe('function for edit, updating status, clear all completed', () => {
+  test('edit function test should edit the name of the task', () => {
+    editTask(2, 'hurray');
+    expect(toDOList).toEqual([{ id: 0, name: 'washing clothes', status: 'pending' }, { id: 1, name: 'dry clothes', status: 'pending' },
+      { id: 2, name: 'hurray', status: 'pending' }]);
+  });
+  test('updateStatus function should update the status of the task', () => {
+    updateStatus(2, 'completed');
+    expect(toDOList).toEqual([{ id: 0, name: 'washing clothes', status: 'pending' }, { id: 1, name: 'dry clothes', status: 'pending' },
+      { id: 2, name: 'hurray', status: 'completed' }]);
   });
 });
--- src/todoFunc.js
+++ src/todoFunc.js
@@ -12,3 +12,34 @@ export const addTask = (task) => {
 export const deleteTask = (index) => {
   todos.splice(index, 1);
 };
+
+// edit task function
+
+// mock object of local storage
+export const toDOList = [
+  { id: 0, name: 'washing clothes', status: 'pending' },
+  { id: 1, name: 'dry clothes', status: 'pending' },
+  { id: 2, name: 'iron clothes', status: 'pending' },
+
+];
+
+// edit function
+export const editTask = (taskid, newName) => {
+  toDOList.forEach((index) => {
+    if (index.id === taskid) {
+      index.name = newName;
+    }
+  });
+  return toDOList;
+};
+
+// update status function
+
+export const updateStatus = (taskid, newStatus) => {
+  toDOList.forEach((index) => {
+    if (index.id === taskid) {
+      index.status = newStatus;
+    }
+  });
+  return toDOList;
+};
